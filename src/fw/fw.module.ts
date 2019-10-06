e65b370c70283dfaa05b115080744330c4283156
swethusiast <aitc.dev@gmail.com>
2019-10-06 21:58:27 +0200

    title-bar

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
 import { ContentComponent } from './content/content.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
 
 @NgModule({
-  declarations: [FrameworkBodyComponent, ContentComponent],
+  declarations: [FrameworkBodyComponent, ContentComponent, TitleBarComponent],
   imports: [CommonModule],
   exports: [FrameworkBodyComponent],
 })
